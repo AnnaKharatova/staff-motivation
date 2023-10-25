20ab7aeb5ffd6771a133f29616864dddc674c3fa
Anna Haratova <black-isida@yandex.ru>
2023-10-25 13:10:13 +0300

    refactor(register activation): added info

--- src/components/App/ActivateProfile/ActivateProfile.js
+++ src/components/App/ActivateProfile/ActivateProfile.js
@@ -6,14 +6,13 @@ function ActivateProfile() {
 	const navigate = useNavigate();
 	const { uid, token } = useParams();
 
-	console.log(uid, token);
 	// http://localhost:3000/activate/MTM/bwcmyz-1781c61a7b00f62103bcf44a10c2db7c
 
 	useEffect(() => {
+		console.log(uid, token);
 		activateRegister(uid, token)
 			.then(() => {
 				navigate('/signin');
-				console.log('активация успешна');
 			})
 			.catch((res) => {
 				if (res === 500) {
@@ -24,7 +23,7 @@ function ActivateProfile() {
 			});
 	}, [navigate, uid, token]);
 
-	return <div>{}</div>;
+	return <div>Активация профиля. Перенаправляем на страницу входа</div>;
 }
 
 export default ActivateProfile;
